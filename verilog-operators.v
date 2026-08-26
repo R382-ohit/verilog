20bd302b45a584ef802dee981b53722add9ee7a1
Rohit Kumar <rohitkumar178.24@kgpian.iitkgp.ac.in>
2026-08-27 05:21:24 +0530

    Verilog Operators

---
+++ verilog-operators.v
@@ -0,0 +1,68 @@
+// Arithmetic Operators
+// unary means operating on a single data item
+// binary means operating on two data items
+/*
++ --- unary(sign) plus
+- --- unary(sign) minus
++ --- binary plus(add)
+- --- binary minus(subtraction)
+* --- multiply
+/ --- divide
+% --- moodulus
+** --- exponentiation
+*/
+
+
+// Logical operators
+/*
+! Logical negation
+&& Logical AND
+|| Logical OR
+*/ 
+
+// value 0 is treated as false but any nonzero value is treated as true
+
+// Relatinoal operators operate in numbers and return boolean result 
+/*
+!=
+==
+>=
+<=
+>
+<
+*/
+
+// Bitwise operators -->> return a value that is also bit 
+/*
+~ Not
+& and
+| or
+^ xor
+~^ xnor
+*/
+
+// Reduction Operators-->> they are unary operators
+/*
+& bitwise AND
+| bitwise OR
+~& bitwise NAND
+~| bitwise NOR
+^ bitwise XOR
+~^ bitwise NOR
+*/
+
+// Shift Operators
+/*
+>> shift right
+<< shift left
+>>> arithmetic shift right 
+*/
+
+// Conditional Operators 
+/*cond_expr? true_expr:false_expr;*/
+
+// concatenation operator
+/*{...,...,...,}*/
+
+// Replication Operator
+/*{n{m}}*/
