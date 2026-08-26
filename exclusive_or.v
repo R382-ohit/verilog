f9716b349c39c0c790698e4618f52da3fef3871b
Rohit Kumar <rohitkumar178.24@kgpian.iitkgp.ac.in>
2026-08-27 03:40:26 +0530

    added logical comments

--- exclusive_or.v
+++ exclusive_or.v
@@ -1,4 +1,11 @@
 `timescale 10ns/1ns
+// this too has some proper pattern into it
+// it goes like ------------>>>>>
+// `timescale <reference_time_unit>/<time_precision>
+
+// it is better to have the delay declaration at the beginning
+//  of the module as when working with large projects we need 
+// to have varying delays for different modules so define then at the top itself 
 
 module exclusive_or(f,a,b);
     input a,b;
