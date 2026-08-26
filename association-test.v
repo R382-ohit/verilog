fb21e83b320797537f412c2d044fee28cbbd2cc8
Rohit Kumar <rohitkumar178.24@kgpian.iitkgp.ac.in>
2026-08-27 04:00:10 +0530

    Explicit v/s Positional association

---
+++ association-test.v
@@ -0,0 +1,19 @@
+module testbench;
+    reg X1, X2, X3, X4, X5, X6;
+    wire OUT;
+    
+    association DUT(.Y(OUT), .A(X1), .B(X2), .C(X3), .D(X4), .E(X5), .F(X6));
+    
+    initial begin
+        $dumpfile("association.vcd");
+        $dumpvars(0, testbench);
+        
+        $monitor($time, " X1=%b, X2=%b, X3=%b, X4=%b, X5=%b, X6=%b, OUT=%b", X1, X2, X3, X4, X5, X6, OUT);
+        
+        #5 X1=0; X2=0; X3=0; X4=1; X5=0; X6=0;
+        #5 X1=0; X3=1;
+        #5 X1=1; X3=0;
+        #5 X6=1;
+        #5 $finish;
+    end
+endmodule
---
+++ association.v
@@ -0,0 +1,9 @@
+module association(A,B,C,D,E,F,Y);
+    input A,B,C,D,E,F;
+    output Y;
+    wire t1,t2,t3,Y;
+    nand  G1 (t1,A,B);
+    and  G2(t2,C,~B,D);
+    nor  G3(t3,E,F);
+    nand  G4(Y,t1,t2,t3);
+endmodule
