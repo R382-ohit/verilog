b2e9003845deab13b98ff02825139bc8964d5d59
Rohit Kumar <rohitkumar178.24@kgpian.iitkgp.ac.in>
2026-08-27 04:09:39 +0530

    updating the latest files

--- association-test.v
+++ association-test.v
@@ -7,7 +7,7 @@ module testbench;
     initial begin
         $dumpfile("association.vcd");
         $dumpvars(0, testbench);
-        
+         
         $monitor($time, " X1=%b, X2=%b, X3=%b, X4=%b, X5=%b, X6=%b, OUT=%b", X1, X2, X3, X4, X5, X6, OUT);
         
         #5 X1=0; X2=0; X3=0; X4=1; X5=0; X6=0;
